9a6b46ba189a496fff5038e79fe2b70449a300f7
Manasa Rao <drmanasa18.rao@gmail.com>
2020-01-07 11:34:50 +0100

    adds input feilds

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
 import React,{ Component} from "react";
 class App extends Component {
   render(){
-    return <div></div>;
+    return (
+    <form>
+      <lable htmlFor="weight">Weight</lable>
+      <input
+      type="number"
+      required
+      placeholder="Weight in Kgs"
+      name="weight"
+      id="weight"
+      />
+      <lable htmlFor="height">height</lable>
+      <input
+      type="number"
+      required
+      placeholder="Height in cm"
+      name="height"
+      id="height"
+      />
+      <button>Calculator BMI</button>
+    </form>
+    );
   }
 }
 
