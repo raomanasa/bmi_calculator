688322ff0ab1fc6b235bcfe672ca909bf67483fb
Manasa Rao <drmanasa18.rao@gmail.com>
2020-01-07 13:14:28 +0100

    adds Message component

---
+++ src/components/Message.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+const Message = props  => {
+    return(
+        <p id='bmi-message'>
+            You are {props.bmiMessage} with a BMI of {props.bmiValue}
+        </p>
+    );
+};
+export default Message;
